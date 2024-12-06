ce6a9d78e3e32efaa9d76f3cddedf352e3d9af87
Edmitsu <ed.barcauscas@gmail.com>
2024-12-06 15:38:38 -0300

    first commit

---
+++ Models/EspecialidadeModel.cs
@@ -0,0 +1,6 @@
+public class Especialidade
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string TipoDocumento { get; set; }
+}
---
+++ Models/ProfissionalModel.cs
@@ -0,0 +1,8 @@
+public class ProfissionalModel
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Especialidade { get; set; }
+    public required string TipoDocumento { get; set; }
+    public required string NumeroDocumento { get; set; }
+}
